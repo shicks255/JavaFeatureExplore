18116e51573f1e9ee976ce8c39baf3beb660f68f
shicks <shicks255@yahoo.com>
2019-10-13 15:42:42 -0500

    more futures

--- src/main/java/Concurrency/Executors/Executors.java
+++ src/main/java/Concurrency/Executors/Executors.java
@@ -1,38 +1,65 @@
 package Concurrency.Executors;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Executors
 {
 
     public static void main(String[] args)
     {
+        doSync();
+        doAsync();
+    }
 
-        CompletableFuture<String> fiture = doFuture();
+    private static void doSync()
+    {
+        long current = System.currentTimeMillis();
+        CompletableFuture<String> future = doFuture()
+                .thenCompose((f1) -> doFuture())
+                .thenCompose((f2) -> doFuture());
 
-        while (!fiture.isDone())
-            System.out.println("waiting...");
-
-        try{
-            System.out.println("Finally done, the answer is " + fiture.get());
-        }
-        catch (Exception e)
+        try
+        {
+            System.out.println(future.get());
+        } catch (Exception e)
         {}
+        System.out.println("Finished synchronously in " + (System.currentTimeMillis() - current));
+    }
+
+    private static void doAsync()
+    {
+        long current = System.currentTimeMillis();
+
+//        CompletableFuture<Void> allFutures =
+//                CompletableFuture.allOf(
+//                        doFuture(),
+//                        doFuture(),
+//                        doFuture()
+//                );
+//        try{
+//            allFutures.get();
+//        } catch (Exception e)
+//        {}
+
+        String answer = Stream.of(doFuture(), doFuture(), doFuture())
+                .map(CompletableFuture::join)
+                .collect(Collectors.joining());
+
+        System.out.println("Finished asynchronously in " + (System.currentTimeMillis() - current));
     }
 
     private static CompletableFuture<String> doFuture()
     {
 
-        CompletableFuture<String> future = new CompletableFuture<>();
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
 
-        java.util.concurrent.Executors.newCachedThreadPool().submit(() -> {
-            try {
+            try{
                 Thread.sleep(1000);
-            }
-            catch (InterruptedException e)
+            } catch (Exception e)
             {}
-
-            future.complete("Finished");
+            return "hello";
         });
 
 
