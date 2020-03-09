9234cae0a492ba1c073d5f541fa2be6e9e33ebdd
shicks <shicks255@yahoo.com>
2020-03-08 22:15:22 -0500

    working on combining functional interfaces

---
+++ src/main/java/ModernJava/ChainingInterfaces.java
@@ -0,0 +1,55 @@
+package ModernJava;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class ChainingInterfaces {
+
+    public static void main(String[] args) {
+        List<Function<Double, Double>> functions = new ArrayList<>();
+        functions.add(x -> x * .12);
+        functions.add(x -> x * .93);
+
+
+        Double nextYear = getBonusCalculator()
+                .startAction(124_080.00)
+                .midAction(functions)
+                .endAction();
+
+        Double thisYearBonus = getBonusCalculator()
+                .startAction(56_000.00)
+                .midAction(functions)
+                .endAction();
+
+        System.out.println(nextYear);
+        System.out.println(thisYearBonus);
+    }
+
+    public static InitialValue getBonusCalculator() {
+        return startValue -> {
+            return (List<Function<Double, Double>> consumers) -> {
+                double intermediateValue = startValue;
+                for (Function<Double, Double> consumer : consumers)
+                    intermediateValue = consumer.apply(intermediateValue);
+                double finalValue = intermediateValue;
+                return () -> finalValue;
+            };
+        };
+    }
+
+    @FunctionalInterface
+    public interface InitialValue {
+        MiddleAction startAction(double start);
+    }
+
+    @FunctionalInterface
+    public interface MiddleAction {
+        TerminalAction midAction(List<Function<Double, Double>> functions);
+    }
+
+    @FunctionalInterface
+    public interface TerminalAction {
+        double endAction();
+    }
+}
